199a26c37316d8863710ce3b4f10f99d60b06fbc
Daryll Ko <daryll.ko@gmail.com>
2024-08-16 00:29:39 +0800

    update: remove newline

--- src/main.rs
+++ src/main.rs
@@ -46,8 +46,8 @@ mod tests {
             val: 5,
             right: Box::new(Nil),
         };
-        assert_eq!(l.find(0), None);
 
+        assert_eq!(l.find(0), None);
         assert_eq!(l.find(5), Some(5));
     }
 }
