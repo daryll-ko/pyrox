f6d42e349058f378fa147ded628fa3c144cd9e09
Daryll Ko <daryll.ko@gmail.com>
2024-08-16 00:03:29 +0800

    update: use `enum` instead of `struct`

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,19 @@
-struct Node {
-    val: i64,
+use crate::LList::{Nil, Node};
+
+// we need enum to define Nil (?)
+enum LList {
+    Node(Box<LList>, i64, Box<LList>),
+    Nil,
+}
+
+fn get_value(l: LList) -> i64 {
+    match l {
+        Node(_, val, _) => val,
+        Nil => 0,
+    }
 }
 
 fn main() {
-    let n1 = Node { val: 2024 };
-    println!("{}", n1.val);
+    let n1 = Node(Box::new(Nil), 2024, Box::new(Nil));
+    println!("{}", get_value(n1));
 }
