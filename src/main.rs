e04495bfc7c78f9057e8ba16bfaacbe0813f6643
Daryll Ko <daryll.ko@gmail.com>
2024-08-16 00:22:15 +0800

    add: implement `find` function for `LList`

--- src/main.rs
+++ src/main.rs
@@ -2,18 +2,46 @@ use crate::LList::{Nil, Node};
 
 // we need enum to define Nil (?)
 enum LList {
-    Node(Box<LList>, i64, Box<LList>),
+    Node {
+        left: Box<LList>,
+        val: i64,
+        right: Box<LList>,
+    },
     Nil,
 }
 
-fn get_value(l: LList) -> i64 {
-    match l {
-        Node(_, val, _) => val,
-        Nil => 0,
+impl LList {
+    fn find(&self, target: i64) -> Option<i64> {
+        match self {
+            Node {
+                left: _,
+                val,
+                right,
+            } => {
+                if target == *val {
+                    Some(*val)
+                } else {
+                    Self::find(&right, target)
+                }
+            }
+            Nil => None,
+        }
     }
 }
 
 fn main() {
-    let n1 = Node(Box::new(Nil), 2024, Box::new(Nil));
-    println!("{}", get_value(n1));
+    let n1 = Node {
+        left: Box::new(Nil),
+        val: 2024,
+        right: Box::new(Nil),
+    };
+
+    if let Node {
+        left: _,
+        val,
+        right: _,
+    } = n1
+    {
+        println!("{}", val);
+    }
 }
