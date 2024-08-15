29b9073df3ddf08f558e3aba508d74004c1f578a
Daryll Ko <daryll.ko@gmail.com>
2024-08-16 00:28:57 +0800

    test: add basic tests for `find`

--- src/main.rs
+++ src/main.rs
@@ -29,6 +29,29 @@ impl LList {
     }
 }
 
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn find_works_on_empty_list() {
+        let l = Nil;
+        assert_eq!(l.find(0), None);
+    }
+
+    #[test]
+    fn find_works_on_list_with_one_element() {
+        let l = Node {
+            left: Box::new(Nil),
+            val: 5,
+            right: Box::new(Nil),
+        };
+        assert_eq!(l.find(0), None);
+
+        assert_eq!(l.find(5), Some(5));
+    }
+}
+
 fn main() {
     let n1 = Node {
         left: Box::new(Nil),
