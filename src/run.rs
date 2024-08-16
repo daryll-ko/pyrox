f6861448c5a8f8ce61c594e8492b99da36ad4642
Daryll Ko <daryll.ko@gmail.com>
2024-08-16 16:43:36 +0800

    update: extract `run` functions into `run.rs`

---
+++ src/run.rs
@@ -0,0 +1,35 @@
+use std::fs;
+use std::io::{self, Write};
+
+pub fn run_file(file_path: &str) -> () {
+    println!("Running file [ {file_path} ]...");
+    let contents =
+        fs::read_to_string(file_path).unwrap_or("(｡>﹏<) couldn't read file...".to_string());
+    run(&contents);
+}
+
+pub fn run_repl() -> () {
+    println!("Running REPL...");
+    loop {
+        print!("Arona> ");
+        io::stdout()
+            .flush()
+            .expect("(｡>﹏<) couldn't flush stdout...");
+
+        let mut line = String::new();
+        io::stdin()
+            .read_line(&mut line)
+            .expect("(｡>﹏<) couldn't read a line...");
+        let line = line.trim();
+
+        if line.len() > 0 {
+            run(&line);
+        } else {
+            break;
+        }
+    }
+}
+
+fn run(code: &str) -> () {
+    println!("Running:\n{code}")
+}
